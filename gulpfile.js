ed0b0c419f312b259ad05c92aff1e0b6a4b91a31
sverevkin25@gmail.com <82106610Vereva>
2018-12-11 14:03:28 +0200

    done

--- gulpfile.js
+++ gulpfile.js
@@ -15,122 +15,133 @@ const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
 
 gulp.task('sass', function () {
-    return gulp.src('scss/style.scss')
-        .pipe(plumber())
-        .pipe(sourceMaps.init())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 version']
-        }))
-        .pipe(sourceMaps.write())
-        .pipe(gulp.dest('build/css'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('scss/style.scss')
+      .pipe(plumber())
+      .pipe(sourceMaps.init())
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: ['last 2 version']
+      }))
+      .pipe(sourceMaps.write())
+      .pipe(gulp.dest('build/css'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('html', function () {
-    return gulp.src('*.html')
-        .pipe(gulp.dest('build'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('*.html')
+      .pipe(gulp.dest('build'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('js', function () {
-    return gulp.src('js/**/*.js')
-        .pipe(gulp.dest('build/js'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('js/**/*.js')
+      .pipe(gulp.dest('build/js'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('css', function () {
-    return gulp.src('css/**/*.css')
-        .pipe(gulp.dest('build/css'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('css/**/*.css')
+      .pipe(gulp.dest('build/css'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('allimg', function () {
-    return gulp.src('img/**/*.{png,jpg}')
-        .pipe(gulp.dest('build/img'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('img/**/*.{png,jpg}')
+      .pipe(gulp.dest('build/img'))
+      .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('images', function () {
-    return gulp.src('build/img/**/*.{png,jpg}')
-        .pipe(imagemin([
-            imagemin.jpegtran({progressive: true}),
-            imageminJpegRecompress({
-                loops: 5,
-                min: 65,
-                max: 70,
-                quality: 'medium'
-            }),
-            imagemin.optipng({optimizationLevel: 3}),
-            pngquant({quality: '65-70', speed: 5})
-        ]))
-        .pipe(gulp.dest('build/img'));
+  return gulp.src('build/img/**/*.{png,jpg}')
+      .pipe(imagemin([
+          imagemin.jpegtran({progressive: true}),
+          imageminJpegRecompress({
+            loops: 5,
+            min: 65,
+            max: 70,
+            quality: 'medium'
+          }),
+          imagemin.optipng({optimizationLevel: 3}),
+          pngquant({quality: '65-70', speed: 5})
+      ]))
+      .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('svg', function () {
-    return gulp.src('img/**/*.svg')
-        .pipe(svgmin({
-            js2svg: {
-                pretty: true
-            }
-        }))
-        .pipe(cheerio({
-            run: function ($) {
-                $('[fill]').removeAttr('fill');
-                $('[stroke]').removeAttr('stroke');
-                $('[style]').removeAttr('style');
-            },
-            parserOptions: {xmlMode: true}
-        }))
-        .pipe(replace('&gt;', '>'))
-        // build svg sprite
-        .pipe(svgSprite({
-            mode: {
-                symbol: {
-                    sprite: "sprite.svg"
-                }
-            }
-        }))
-        .pipe(gulp.dest('build/img'));
+  return gulp.src('img/**/*.svg')
+      .pipe(svgmin({
+        js2svg: {
+          pretty: true
+        }
+      }))
+      .pipe(cheerio({
+        run: function ($) {
+          $('[fill]').removeAttr('fill');
+          $('[stroke]').removeAttr('stroke');
+          $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+      }))
+      .pipe(replace('&gt;', '>'))
+      // build svg sprite
+      .pipe(svgSprite({
+        mode: {
+          symbol: {
+            sprite: "sprite.svg"
+          }
+        }
+      }))
+      .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('serve', function () {
-    browserSync.init({
-        server: "build"
-    });
+  browserSync.init({
+    server: "build"
+  });
 
-    gulp.watch("scss/**/*.scss", ["sass"]);
-    gulp.watch("*.html", ["html"]);
-    gulp.watch("js/**/*.js", ["js"]);
-    gulp.watch("css/**/*.css", ["css"]);
-    gulp.watch("img/**/*.{png,jpg}", ["allimg"]);
-    gulp.watch("img/**/*.{svg}", ["svg"]);
+  gulp.watch("scss/**/*.scss", ["sass"]);
+  gulp.watch("*.html", ["html"]);
+  gulp.watch("js/**/*.js", ["js"]);
+  gulp.watch("css/**/*.css", ["css"]);
+  gulp.watch("img/**/*.{png,jpg}", ["allimg"]);
+  gulp.watch("img/**/*.{svg}", ["svg"]);
 });
 
 gulp.task('copy', function () {
-    return gulp.src([
-        'img/**',
-        'js/**',
-        'css/**',
-        '*.html'
-    ], {
-        base: '.'
-    })
-        .pipe(gulp.dest('build'));
+  return gulp.src([
+      'img/**',
+      'js/**',
+      'css/**',
+      '*.html'
+  ], {
+    base: '.'
+  })
+      .pipe(gulp.dest('build'));
 
 });
 
 gulp.task('clean', function () {
-    return del('build');
+  return del('build');
 });
 
 gulp.task('build', function (fn) {
-    run(
-        'clean',
-        'copy',
-        'sass',
-        'images',
-        'svg',
-        fn
-    );
+  run(
+    'clean',
+    'copy',
+    'sass',
+    'images',
+    'svg',
+    fn
+  );
 });
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
 $(document).ready(function () {
 
-    $('.slider .owl-carousel').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        dots: false,
-        navText: [],
-        responsive: {
-            0: {
-                items: 1,
-                dots: true,
-                nav: false
-            },
-            480: {
-                items: 1
-            }
-        }
-    })
+  $('.slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots: false,
+    navText: [],
+    responsive:{
+      0:{
+        items:1,
+        dots: true,
+        nav: false
+      },
+      480:{
+        items:1
+      }
+    }
+  });
+
+  $('.carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    navText: [],
+    responsive:{
+      0:{
+        items: 1
+      },
+      480:{
+        items: 2
+      },
+      680: {
+        items: 3
+      },
+      1024: {
+        items: 4
+      }
+    }
+  });
+
 });
